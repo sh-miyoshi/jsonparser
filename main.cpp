07919cb54950f4531a5f5224fb1179de5e52db4e
Shunsuke Miyoshi <s.miyoshi1991@gmail.com>
2019-10-28 23:05:58 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,122 @@
+#include <map>
 #include <stdio.h>
 #include <string>
+#include <vector>
 
-int main(){
-	std::string msg="hello, world";
-	printf("%s\n",msg.c_str());
+union json {
+    std::string str;
+    std::map<std::string, std::string> obj;
+    std::vector<std::string> ary;
+
+    json() {}
+};
+
+void parse_value(std::string *str);
+void parse_array(std::string *str);
+void parse_object(std::string *str);
+void parse_string(std::string *str);
+
+void remove(std::string *str) {
+    while ((*str)[0] == ' ' || (*str)[0] == '\n') {
+        (*str).erase(str->begin());
+    }
+}
+
+void parse_array(std::string *str) {
+    std::string val;
+    int index = 0;
+    while (!str->empty()) {
+        remove(str);
+        switch ((*str)[0]) {
+        case '{':
+            parse_object(str);
+            break;
+        case ',':
+            printf("value%d: %s\n", index, val.c_str());
+            val = "";
+            index++;
+            break;
+        case '"':
+            break;
+        case ']':
+            printf("value%d: %s\n", index, val.c_str());
+            return;
+        default:
+            val += (*str)[0];
+            break;
+        }
+        (*str).erase(str->begin());
+    }
+}
+
+void parse_object(std::string *str) {
+    std::string key;
+    while (!str->empty()) {
+        remove(str);
+        switch ((*str)[0]) {
+        case '{':
+            parse_object(str);
+            break;
+        case '[':
+            parse_array(str);
+            break;
+        case ':':
+            printf("key: %s, ", key.c_str());
+            (*str).erase(str->begin());
+            parse_value(str);
+            break;
+        case ',':
+            key = "";
+            break;
+        case '"':
+            break;
+        case '}':
+            return;
+        default:
+            key += (*str)[0];
+            break;
+        }
+        (*str).erase(str->begin());
+    }
+}
+
+void parse_string(std::string *str) {
+    std::string res;
+    remove(str);
+    while ((*str)[0] != '"') {
+        res += (*str)[0];
+        (*str).erase(str->begin());
+    }
+    printf("value: %s\n", res.c_str());
+}
+
+void parse_value(std::string *str) {
+    remove(str);
+    switch ((*str)[0]) {
+    case '{':
+        (*str).erase(str->begin());
+        parse_object(str);
+        break;
+    case '"':
+        (*str).erase(str->begin());
+        parse_string(str);
+        break;
+    case '[':
+        (*str).erase(str->begin());
+        parse_array(str);
+        break;
+    }
+}
+
+int main() {
+    std::string input;
+    FILE *fp = fopen("input.json", "r");
+    int ch;
+    while ((ch = fgetc(fp)) != EOF) {
+        input += (char)ch;
+    }
+    fclose(fp);
+
+    //puts(input.c_str());
+    parse_value(&input);
 }
